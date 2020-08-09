cc2de3cc638db01807a3ae1ac9e430ed4826427e
Davenel Denis <daveneldenis@gmail.com>
2020-08-09 12:42:34 -0400

    Initial Commit

--- plots.js
+++ plots.js
@@ -1,37 +1,46 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function init() {
+    var selector = d3.select("#selDataset");
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+    })
+}
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+        PANEL.html("");
+        Object.entries(result).forEach(([key,value])=>{
+            PANEL.append("h6").text(`${key}: ${value}`);
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
+        });
+        
+    });
+}
+function buildPlots(sample){
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        console.log(result);
+        
+    });
 
-// data
-var data = [trace1];
+}
+function optionChanged(newSample) {
+    console.log(newSample);
+    buildMetadata(newSample);
+    buildPlots(newSample);
+}
 
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+init();
