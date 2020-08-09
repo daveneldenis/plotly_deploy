b433a2b2d07339f30f5991861949aed13987263a
Davenel Denis <daveneldenis@gmail.com>
2020-08-09 14:16:56 -0400

    Finished JvaScript Commit

--- plots.js
+++ plots.js
@@ -2,7 +2,6 @@ function init() {
     var selector = d3.select("#selDataset");
 
     d3.json("samples.json").then((data) => {
-        console.log(data);
         var sampleNames = data.names;
         sampleNames.forEach((sample) => {
             selector
@@ -32,13 +31,49 @@ function buildPlots(sample){
         var samples = data.samples;
         var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
         var result = resultArray[0];
-        console.log(result);
-        
-    });
 
+        var otu_ids = result.otu_ids.slice(0,10).reverse();
+        var otu_labels = result.otu_labels.slice(0,10).reverse();
+        var sample_values = result.sample_values.slice(0,10).reverse();
+        otu_ids = otu_ids.map(label => `OTU ${label}`); 
+
+        var data = [{
+            type: 'bar',
+            x: sample_values,
+            y: otu_ids,
+            orientation: 'h',
+            text: otu_labels
+          }];
+          
+        Plotly.newPlot('bar', data);
+
+        var x = result.otu_ids;
+        var text = result.otu_labels;
+        var y = result.sample_values;
+        
+        var trace1 = {
+            x: x,
+            y: y,
+            text: text,
+            mode: 'markers',
+            marker: {
+              color: x,
+              size: y
+            }
+          };
+          
+          var data = [trace1];
+          
+          var layout = {
+            title: 'OTU ID',
+            showlegend: false,
+          };
+          
+          Plotly.newPlot('bubble', data, layout);
+    });
 }
+
 function optionChanged(newSample) {
-    console.log(newSample);
     buildMetadata(newSample);
     buildPlots(newSample);
 }
